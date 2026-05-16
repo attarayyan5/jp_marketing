1afe86b63e1a7596efb59dab50a9fbe406471a94
attarayyan5 <161196357+attarayyan5@users.noreply.github.com>
2026-05-16 12:34:10 +0530

    Update CORS origin for production environment

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: ['jpm-marketing-vpdr-attarayyan5s-projects.vercel.app', 'http://localhost:3000'],
+  origin: ['https://jpm-marketing.vercel.app/', 'http://localhost:3000'],
   credentials: true
 }));
 app.use(express.json());
