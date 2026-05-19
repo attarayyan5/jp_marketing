73f3db4160f7a8fab9204fc0dd5e99ec3351e75d
attarayyan5 <161196357+attarayyan5@users.noreply.github.com>
2026-05-20 01:39:30 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: ['https://jpm-marketing.vercel.app', 'http://localhost:3000'],
+  origin: ['https://jpm-marketing.vercel.app/', 'http://localhost:3000'],
   credentials: true
 }));
 app.use(express.json());
